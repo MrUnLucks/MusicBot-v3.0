e763666f8c41cb3ac1bbda52842fa88a80ac0089
luca-costa <100123915+luca-costa@users.noreply.github.com>
2022-04-22 23:31:57 +0200

    feat: add play command

--- commands/play.js
+++ commands/play.js
@@ -1,6 +1,11 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const ytSearch = require("yt-search");
-const ytdl = require('ytdl-core')
+const ytdl = require('ytdl-core');
+const {
+  createAudioPlayer,
+  createAudioResource,
+  joinVoiceChannel,
+} = require("@discordjs/voice");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -11,9 +16,27 @@ module.exports = {
         .setDescription('Song to be reproduced')
         .setRequired(true)),
   async execute(interaction) {
+    const member = interaction.guild.members.cache.get(interaction.member.user.id);
+    const voiceChannelId = member.voice.channelId;
+    if(voiceChannelId == null){
+      return interaction.reply({content:"You need to be in a voice channel to request a song",ephemeral:true})
+    }
+    const player = createAudioPlayer();
+    player.on("error", (error) => {
+      console.error("Error:", error.message);
+    });
     let userQuery = interaction.options._hoistedOptions[0].value;
     let song = await songFinder(userQuery);
-    await interaction.reply(`You searched for ${song.title}!`);
+    if(ytdl.validateURL(song.url)){
+      const stream = createAudioResource(ytdl(song.url, { filter: 'audioonly' }));
+      joinVoiceChannel({
+        channelId: voiceChannelId,
+        guildId: interaction.guild.id,
+        adapterCreator: interaction.guild.voiceAdapterCreator,
+      }).subscribe(player);
+      player.play(stream);
+    }
+    await interaction.reply({content:`You searched for ${song.title}!`,ephemeral:true});
   },
 };
 
